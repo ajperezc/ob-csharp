c21f3157586224ed6891ebec675e30c511a3f062
ajperezc <54717822+ajperezc@users.noreply.github.com>
2022-07-14 19:29:19 +0200

    data-structure exercise finish

---
+++ data-structure/data-structure/Program.cs
@@ -0,0 +1,33 @@
+﻿// Exercise 1
+
+Client newClient = new Client(
+    "Pepe Botella",
+    666999666,
+    "Calle Flor, 3",
+    "pepe@botella.es",
+    true
+);
+
+Console.WriteLine(newClient);
+
+public struct Client
+{
+    public Client(string clientName, int clientPhone, string clientAddress, string clientEmail, bool clientIsNew)
+    {
+        name = clientName;
+        phone = clientPhone;
+        address = clientAddress;
+        email = clientEmail;
+        isNew = clientIsNew;
+    }
+    
+    public string name { get; set; }
+    public int phone { get; set; }
+    public string address { get; set; }
+    public string email { get; set; }
+    public bool isNew { get; set; }
+
+
+    public override string ToString() => $"Name: {name}\nPhone: {phone}\nAddress: {address}" +
+                                         $"\nEmail: {email}\nNew?: {isNew}";
+}
