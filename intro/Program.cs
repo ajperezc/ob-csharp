05189010c32661464052098a75459a9fbdfd9fdd
ajperezc <54717822+ajperezc@users.noreply.github.com>
2022-07-14 18:11:54 +0200

    intro exercise

---
+++ intro/Program.cs
@@ -0,0 +1,10 @@
+﻿// Exercise 1
+
+Console.Write("Introduce el nombre : ");
+string nombre = Console.ReadLine();
+Console.WriteLine("Hola, tu nombre es " + nombre + ".");
+
+// Exercise 2
+
+string datetime = DateTime.Now.ToString("hh:mm:ss tt");
+Console.WriteLine("La hora actual es : " + datetime);
