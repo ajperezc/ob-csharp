60c335f8bdc1f451ff7f49dc9d41c1c85ed30dc3
ajperezc <54717822+ajperezc@users.noreply.github.com>
2022-07-15 19:18:45 +0200

    Exercise 3 finish

--- loops/Program.cs
+++ loops/Program.cs
@@ -1,43 +1,69 @@
 ﻿// Exercise 1 - While
 
-Console.Write("Insert a int number: ");
+Console.Write("Insert a int number to calculate its multiplication table: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("\n-----" + number + " MULTIPLICATION TABLE ----- \n");
-int i = 1;
-while (i <= 10)
+int num = 1;
+while (num <= 10)
 {
-    int result = i * number;
-    Console.WriteLine(i + " x " + number + " = " + result);
-    i++;
+    int result = num * number;
+    Console.WriteLine(num + " x " + number + " = " + result);
+    num++;
 }
 
 // Exercise 2 - Do While
 
-int j = 1, countNegative = 0, countPositive = 0, countZero = 0;
+int count = 1, countNegative = 0, countPositive = 0, countZero = 0;
 do
 {
-    Console.Write("\nInsert a int number: ");
+    Console.Write("\nInsert a int number to count (negative, zero or positive): ");
     int number2 = Convert.ToInt32(Console.ReadLine());
     if (number2 < 0)
     {
         countNegative++;
-        j++;
+        count++;
     }
     else if (number2 > 0)
     {
         countPositive++;
-        j++;
+        count++;
     }
     else
     {
         countZero++;
-        j++;
+        count++;
     }
     Console.WriteLine("Counter: Negative: " + countNegative + " Positive: " + countPositive + " Zero: " + countZero);
-} while (j <= 10);
+} while (count <= 10);
 
 // Exercise 3 For
 
-     
-     
+Console.WriteLine("\n-----Drawing a rectangle-----\n");
+Console.Write("Insert the height (number): ");
+int height = Convert.ToInt32(Console.ReadLine());
+Console.Write("Insert the width (number): ");
+int width = Convert.ToInt32(Console.ReadLine());
+        
+Console.Write("With filling? (Y/N): ");
+string withFilling = Console.ReadLine();
+Console.Write("How much filling? (number): ");
+int qFilling = Convert.ToInt32(Console.ReadLine());
+
+string filling = new string('*', qFilling);
+
+for (int i = 0; i < height; i++)
+{
+    for (int j = 0; j < width; j++)
+    {
+        if ((withFilling == "N") && (i > 0 && i < height -1) && (j > 0 && j < width -1))
+        {
+            Console.Write(" ");
+        }
+        else
+        {
+            Console.Write(filling);
+        }
+    }
+    Console.Write("\n");
+};
